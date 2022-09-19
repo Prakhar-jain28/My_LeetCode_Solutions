bc4ce5b8fe694c12a7b423c67b010ab6c10e8ef2
Prakhar-jain28 <73034647+Prakhar-jain28@users.noreply.github.com>
2022-09-19 20:03:11 +0530

    new

---
+++ Medium/240_Search_a_2_D_Matrix_II.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int m = 0, n = matrix[0].size() - 1;
+        while (m < matrix.size() && n >= 0)
+        {
+            if (matrix[m][n] == target)
+                return 1;
+            if (matrix[m][n] > target)
+            {
+                n--;
+            }
+            else
+                m++;
+        }
+        return 0;
+    }
+};
