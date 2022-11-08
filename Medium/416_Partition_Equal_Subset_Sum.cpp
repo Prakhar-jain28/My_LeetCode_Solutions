a7eb8d091e8a3f7314a399eb252c2914d67b70eb
Prakhar-jain28 <73034647+Prakhar-jain28@users.noreply.github.com>
2022-11-08 20:03:03 +0530

    new

---
+++ Medium/416_Partition_Equal_Subset_Sum.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+public:
+    bool canPartition(vector<int> &nums)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+        }
+
+        int n = nums.size();
+        if (sum % 2 == 1)
+            return 0;
+        else
+            sum = sum / 2;
+        bool dp[n + 1][sum + 1];
+        for (int i = 0; i < n + 1; i++)
+        {
+            for (int j = 0; j < sum + 1; j++)
+            {
+                if (i == 0)
+                    dp[i][j] = 0;
+                if (j == 0)
+                    dp[i][j] = 1;
+            }
+        }
+        for (int i = 1; i < n + 1; i++)
+        {
+            for (int j = 1; j < sum + 1; j++)
+            {
+                if (j >= nums[i - 1])
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][sum];
+    }
+};
